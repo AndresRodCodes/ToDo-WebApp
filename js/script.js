d3b06ec9a84efa05938a568a40bb7bdf126e3b9e
Andres Rodriguez <andres6459@gmail.com>
2020-01-29 14:47:48 -0800

    Added a dynamic date.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 const list = document.getElementById("list");
 const btnAddTodo = document.getElementById("btn-Add-Todo");
 const userTodo = document.getElementById("input");
+const dateElement = document.getElementById("date");
+
+const options = {weekday: "long", month : "short", day : "numeric"};
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
 let addToDo = () => {
     let userText = userTodo.value;
-    list.insertAdjacentHTML("beforeend", `<li class="user-todo" >${userText}</li>`);
+    if (userText) {
+        list.insertAdjacentHTML("beforeend", `<li class="user-todo" >${userText}</li>`);
+        userTodo.value = "";
+    }
 }
 
 btnAddTodo.addEventListener("click", addToDo);
